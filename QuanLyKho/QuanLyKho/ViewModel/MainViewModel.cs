42b8257b3363b4f6f35f0cdc323cfae30f245748
Ancol <trandieu@gmail.com>
2024-05-04 14:41:40 +0700

    -Build MVVM - Add login page - Add MainResource - Fix issue config material design

---
+++ QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoading = false;
+        public MainViewModel()
+        {
+            if (!IsLoading)
+            {
+                IsLoading = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}

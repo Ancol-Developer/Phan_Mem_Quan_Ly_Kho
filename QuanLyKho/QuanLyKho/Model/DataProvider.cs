d6697dcfb186185c2037f218bc3be0524bd57dd0
Ancol <trandieu@gmail.com>
2024-05-14 21:11:56 +0700

    Add DataProvider class

---
+++ QuanLyKho/QuanLyKho/Model/DataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins { get { if(_ins is null) _ins = new DataProvider(); return _ins; } set { _ins = value; } }
+        public QuanLyKhoAncolEntities DB { get; set; }
+        private DataProvider() 
+        { 
+            DB = new QuanLyKhoAncolEntities();
+        }
+    }
+}

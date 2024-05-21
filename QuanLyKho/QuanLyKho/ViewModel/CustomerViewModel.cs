b5d1f692d0c50e8e2a827da298a1541e7a6ea522
Ancol <trandieu@gmail.com>
2024-05-21 23:57:57 +0700

    CRUD Customer

---
+++ QuanLyKho/QuanLyKho/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,126 @@
+﻿using QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        private ObservableCollection<Customer> _List;
+        public ObservableCollection<Customer> List
+        {
+            get => _List; set { _List = value; OnPropertyChanged(); }
+        }
+
+        private string _DisplayName;
+        public string DisplayName { get => _DisplayName; set { _DisplayName = value; OnPropertyChanged(); } }
+
+        private string _Phone;
+        public string Phone { get => _Phone; set { _Phone = value; OnPropertyChanged(); } }
+
+        private string _Address;
+        public string Address { get => _Address; set { _Address = value; OnPropertyChanged(); } }
+
+        private string _Email;
+        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
+
+        private string _MoreInfo;
+        public string MoreInfo { get => _MoreInfo; set { _MoreInfo = value; OnPropertyChanged(); } }
+
+        private DateTime? _ContractDate;
+        public DateTime? ContractDate { get => _ContractDate; set { _ContractDate = value; OnPropertyChanged(); } }
+
+        private Customer _SelectedItem;
+        public Customer SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    DisplayName = SelectedItem.DisplayName;
+                    Phone = SelectedItem.Phone;
+                    Email = SelectedItem.Email;
+                    Address = SelectedItem.Address;
+                    MoreInfo = SelectedItem.MoreInfo;
+                    ContractDate = SelectedItem.ContractDate;
+                }
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public CustomerViewModel()
+        {
+            List = new ObservableCollection<Customer>(DataProvider.Ins.DB.Customers);
+
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName))
+                    return false;
+
+                return true;
+            },
+            (p) =>
+            {
+                var newCustomer = new Customer()
+                {
+                    DisplayName = DisplayName,
+                    Phone = Phone,
+                    Address = Address,
+                    Email = Email,
+                    MoreInfo = MoreInfo,
+                    ContractDate = ContractDate,
+                };
+
+                //Save to view
+                List.Add(newCustomer);
+
+                //Save to DB
+                DataProvider.Ins.DB.Customers.Add(newCustomer);
+                DataProvider.Ins.DB.SaveChanges();
+            });
+
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(DisplayName)
+                || SelectedItem == null)
+                    return false;
+
+                var check = DataProvider.Ins.DB.Units.Where(x => x.Id == SelectedItem.Id);
+                if (check == null
+                || check.Count() == 0)
+                    return false;
+
+                return true;
+            },
+            (p) =>
+            {
+                var Customer = DataProvider.Ins.DB.Customers.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
+                Customer.DisplayName = DisplayName;
+                Customer.Phone = Phone;
+                Customer.Address = Address;
+                Customer.Email = Email;
+                Customer.MoreInfo = MoreInfo;
+                Customer.ContractDate = ContractDate;
+                //Save to DB
+                DataProvider.Ins.DB.SaveChanges();
+
+                // Update to SelectedItem
+                SelectedItem.DisplayName = DisplayName;
+                SelectedItem.Phone = Phone;
+                SelectedItem.Address = Address;
+                SelectedItem.Email = Email;
+                SelectedItem.MoreInfo = MoreInfo;
+                SelectedItem.ContractDate = ContractDate;
+            });
+        }
+    }
+}
